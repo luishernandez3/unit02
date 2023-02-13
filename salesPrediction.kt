6c750a63eaf87f7619c8ccdc43cce996f7da1464
luishernandez3 <123015178+luishernandez3@users.noreply.github.com>
2023-02-12 22:58:13 -0600

    Create salesPrediction.kt

---
+++ salesPrediction.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val totalSales = 4.6
+    val eastCoastPercentage = 0.62
+    val eastCoastSales = totalSales * eastCoastPercentage
+
+    println("The east coast division will get ${eastCoastSales} million in sales this year.")
+}

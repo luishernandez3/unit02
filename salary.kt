a1bc1effbe9777cc4e9e874a4915cad7394489ac
luishernandez3 <123015178+luishernandez3@users.noreply.github.com>
2023-02-12 23:11:09 -0600

    Create salary.kt

---
+++ salary.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var name = "My name is Jose Luis, "
+    val age = "my age in 2023 is 19 "
+    val annualPay = "and I'll be making 1,000,000 per year."
+    
+    println (name + age + annualPay)
+
+   
+}

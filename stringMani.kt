37d8aad49178a1f68b504c490968a5f16753f076
luishernandez3 <123015178+luishernandez3@users.noreply.github.com>
2023-02-12 23:42:22 -0600

    Create stringMani.kt

---
+++ stringMani.kt
@@ -0,0 +1,9 @@
+fun main() {
+    print("Please, enter the name of your favorite city: ")
+    var city= "Morelia"
+   
+    println("The number of characters in the city name: ${city.length}")
+    println("The name of the city in all uppercase letters: ${city.toUpperCase()}")
+    println("The name of the city in all lowercase letters: ${city.toLowerCase()}")
+    println("The first character in the name of the city: ${city[0]}")
+}
